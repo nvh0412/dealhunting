4ed2323396aed8d138117f7d60db4003172c81b1
Hoa Nguyen <nvh0412@gmail.com>
2016-11-12 09:15:47 +0700

    feature: Implemnt tablayout and viewpager

--- app/src/main/java/com/vagabond/dealhunting/MainActivity.java
+++ app/src/main/java/com/vagabond/dealhunting/MainActivity.java
@@ -1,22 +1,95 @@
 package com.vagabond.dealhunting;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
   private static final String LOG_TAG = MainActivity.class.getSimpleName();
-  private Toolbar mToolbar;
-  private RecyclerView mDealRecyclerView;
+  private ViewPager mPager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    Log.d(LOG_TAG, "onCreate");
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-    mToolbar = (Toolbar) findViewById(R.id.toolbar);
+    ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+    if (viewPager != null) {
+      setupViewPager(viewPager);
+    }
 
-    mDealRecyclerView = (RecyclerView) findViewById(R.id.deal_recycler_view);
+    TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+    tabLayout.setupWithViewPager(viewPager);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main, menu);
+    return true;
+  }
+
+  @Override
+  public void onBackPressed() {
+    Log.d(LOG_TAG, "onBackPressed: Pager position" + mPager.getCurrentItem());
+    if (mPager.getCurrentItem() == 0) {
+      super.onBackPressed();
+    } else {
+      mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+    }
+  }
+
+  private void setupViewPager(ViewPager viewPager) {
+    DealFragment dealFragment = new DealFragment();
+    DealFragment dealFragment2 = new DealFragment();
+    DealFragment dealFragment3 = new DealFragment();
+
+    PagerFragmentAdapter adapter = new PagerFragmentAdapter(getSupportFragmentManager());
+    adapter.addFragment(dealFragment, "Hoa dep trai");
+    adapter.addFragment(dealFragment2, "Hoa dep trai");
+    adapter.addFragment(dealFragment3, "Hoa dep trai");
+
+    viewPager.setAdapter(adapter);
+  }
+
+  private class PagerFragmentAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragments = new ArrayList<>();
+    private final List<String> mFragmentTitles = new ArrayList<>();
+
+    PagerFragmentAdapter(FragmentManager fm) {
+      super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+      return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+      return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return mFragmentTitles.get(position);
+    }
+
+    void addFragment(Fragment fragment, String title) {
+      mFragments.add(fragment);
+      mFragmentTitles.add(title);
+    }
   }
 }
