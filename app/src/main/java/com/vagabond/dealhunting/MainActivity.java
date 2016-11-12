1836f6da13785bccc9fe8f47ca16127e907b69d8
Hoa Nguyen <nvh0412@gmail.com>
2016-11-12 14:03:07 +0700

    feature: Implement DrawLayout and Navigation Drawer
    - Create layout

--- app/src/main/java/com/vagabond/dealhunting/MainActivity.java
+++ app/src/main/java/com/vagabond/dealhunting/MainActivity.java
@@ -1,15 +1,24 @@
 package com.vagabond.dealhunting;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.Thing;
+
+import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +26,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
   private static final String LOG_TAG = MainActivity.class.getSimpleName();
   private ViewPager mPager;
+  private DrawerLayout mDrawerLayout;
+  private ActionBarDrawerToggle mDrawerToggle;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +37,68 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    ViewPager viewPager = (ViewPager)findViewById(R.id.viewpager);
+    ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
     if (viewPager != null) {
       setupViewPager(viewPager);
     }
 
-    TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+    TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
     tabLayout.setupWithViewPager(viewPager);
+
+    mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+    mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_search_white_24dp, R.string.drawer_open, R.string.drawer_close) {
+      @Override
+      public void onDrawerClosed(View drawerView) {
+        super.onDrawerClosed(drawerView);
+        invalidateOptionsMenu();
+      }
+
+      @Override
+      public void onDrawerOpened(View drawerView) {
+        super.onDrawerOpened(drawerView);
+        invalidateOptionsMenu();
+      }
+    };
+
+    mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    getSupportActionBar().setHomeButtonEnabled(true);
+  }
+
+
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    // Sync the toggle state after onRestoreInstanceState has occurred.
+    mDrawerToggle.syncState();
+  }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    mDrawerToggle.onConfigurationChanged(newConfig);
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    return super.onPrepareOptionsMenu(menu);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     getMenuInflater().inflate(R.menu.main, menu);
     return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    if (mDrawerToggle.onOptionsItemSelected(item)) {
+      return true;
+    }
+    
+    return super.onOptionsItemSelected(item);
   }
 
   @Override
@@ -62,6 +122,32 @@ public class MainActivity extends AppCompatActivity {
     adapter.addFragment(dealFragment3, "Hoa dep trai");
 
     viewPager.setAdapter(adapter);
+  }
+
+  /**
+   * ATTENTION: This was auto-generated to implement the App Indexing API.
+   * See https://g.co/AppIndexing/AndroidStudio for more information.
+   */
+  public Action getIndexApiAction() {
+    Thing object = new Thing.Builder()
+        .setName("Main Page") // TODO: Define a title for the content shown.
+        // TODO: Make sure this auto-generated URL is correct.
+        .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+        .build();
+    return new Action.Builder(Action.TYPE_VIEW)
+        .setObject(object)
+        .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+        .build();
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
   }
 
   private class PagerFragmentAdapter extends FragmentPagerAdapter {
